439d9b137330a1d2bd9857c3cde3fa2539397de8
lukevanlukevan <lukevrensburg@gmail.com>
2025-05-13 15:13:54 +0200

    added lock in keymaps (disabled arrow keys)

--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -3,3 +3,10 @@
 -- Add any additional keymaps here
 
 vim.api.nvim_set_keymap("i", "jj", "<Esc>", { noremap = false })
+
+-- time to lock in:
+
+vim.keymap.set("n", "<Left>", ':echoe "Use h"<CR>', { noremap = true, silent = true })
+vim.keymap.set("n", "<Right>", ':echoe "Use l"<CR>', { noremap = true, silent = true })
+vim.keymap.set("n", "<Up>", ':echoe "Use k"<CR>', { noremap = true, silent = true })
+vim.keymap.set("n", "<Down>", ':echoe "Use j"<CR>', { noremap = true, silent = true })
