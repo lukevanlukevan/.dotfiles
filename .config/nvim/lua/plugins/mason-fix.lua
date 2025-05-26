66acf08d1b56fcbb07b2aac9aed3fef7fe969e0e
lukevanlukevan <lukevrensburg@gmail.com>
2025-05-26 16:07:16 +0200

    maybe automatic enable?

--- .config/nvim/lua/plugins/mason-fix.lua
+++ .config/nvim/lua/plugins/mason-fix.lua
@@ -2,3 +2,6 @@ return {
   { "mason-org/mason.nvim", version = "~1.0.0" },
   { "mason-org/mason-lspconfig.nvim", version = "~1.0.0" },
 }
+-- require("mason-lspconfig").setup({
+--   automatic_enable = true,
+-- })
