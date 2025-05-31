23498f4439f8da9c21afb06b190a80ec5ffefaff
lukevanlukevan <lukevrensburg@gmail.com>
2025-05-31 09:58:43 +0200

    added frankly

--- .config/nvim/lua/plugins/todo-float.lua
+++ .config/nvim/lua/plugins/todo-float.lua
@@ -1,7 +1,8 @@
 return {
-  dir = "D:/Code/nvim-plugins/floatingtodo.nvim/",
+  dir = "~/Code/frankly.nvim",
   opts = {
-    target_file = "C:/Users/PIC-TWO/todo.md",
+    target_dir = "~/todo",
+    target_file = "~/todo.md",
     border = "single", -- single, rounded, etc.
     width = 0.8, -- width of window in % of screen size
     height = 0.8, -- height of window in % of screen size
