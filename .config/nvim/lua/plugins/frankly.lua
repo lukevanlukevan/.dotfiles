c2213bcc55bd9be09d5e6534699ac7921beae750
Luke Van <lukevrensburg@gmail.com>
2025-06-04 20:07:26 +0200

    ammended config for os specific

--- .config/nvim/lua/plugins/frankly.lua
+++ .config/nvim/lua/plugins/frankly.lua
@@ -1,29 +1,16 @@
+local non_win = vim.fn.has("macunix")
+if non_win then
+  PLUGIN_DIR = "~/Code/frankly.nvim"
+  TODO_DIR = "~/todo"
+else
+  PLUGIN_DIR = "~/Code/frankly.nvim"
+  TODO_DIR = "~/todo"
+end
 return {
   "frankly.nvim", -- Assuming this is the name of your plugin
-  -- dir = "~/Code/frankly.nvim",
-  dir = "D:/Code/frankly.nvim",
-  -- dependencies = {
-  --   -- Correct way to configure nvim-treesitter as a dependency
-  --   {
-  --     "nvim-treesitter/nvim-treesitter",
-  --     build = ":TSUpdate", -- This command ensures parsers are installed
-  --     config = function()
-  --       require("nvim-treesitter.configs").setup({
-  --         ensure_installed = { "markdown", "markdown_inline", "lua" }, -- Crucial: ensures markdown parsers are installed
-  --         highlight = {
-  --           enable = true, -- Enables Tree-sitter based highlighting
-  --         },
-  --         indent = {
-  --           enable = true, -- Enables Tree-sitter based indentation
-  --         },
-  --         -- You can add more configurations here for other languages or features
-  --       })
-  --     end,
-  --   },
-  -- },
+  dir = PLUGIN_DIR, -- dir = "D:/Code/frankly.nvim",
   opts = {
-    -- target_dir = "~/todo",
-    target_dir = "C:/Users/PIC-TWO/Documents/todo",
+    target_dir = TODO_DIR,
     border = "rounded", -- single, rounded, etc.
     width = 85, -- width in columns
     height = 45, -- height in lines
