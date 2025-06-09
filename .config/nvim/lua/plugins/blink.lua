634e063a97d434a97da44a07e540d61ca1387425
lukevanlukevan <lukevrensburg@gmail.com>
2025-06-09 10:36:11 +0200

    link added

--- .config/nvim/lua/plugins/blink.lua
+++ .config/nvim/lua/plugins/blink.lua
@@ -6,6 +6,7 @@ return {
     { "L3MON4D3/LuaSnip", version = "v2.*" },
   },
   opts = {
+    -- https://cmp.saghen.dev/configuration/keymap
     completion = {
       list = {
         selection = { preselect = false, auto_insert = false },
