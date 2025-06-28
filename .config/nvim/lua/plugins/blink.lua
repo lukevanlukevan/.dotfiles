eaf2329f4372ff938c09e67db5fc47266bf251f5
Luke Van <lukevrensburg@gmail.com>
2025-06-28 17:17:31 +0200

    super tab

--- .config/nvim/lua/plugins/blink.lua
+++ .config/nvim/lua/plugins/blink.lua
@@ -6,12 +6,25 @@ return {
     { "L3MON4D3/LuaSnip", version = "v2.*" },
   },
   opts = {
+
+    keymap = {
+      preset = "super-tab",
+    },
     -- https://cmp.saghen.dev/configuration/keymap
     completion = {
+      menu = {
+        border = "rounded",
+      },
       list = {
         selection = { preselect = false, auto_insert = false },
       },
     },
+    -- normal
+    -- completion = {
+    --   list = {
+    --     selection = { preselect = false, auto_insert = false },
+    --   },
+    -- },
     sources = {
       default = { "lsp", "path", "snippets", "buffer" },
     },
