08e963875fdc9e4f9f359d9efdd5f131c79be380
lukevanlukevan <lukevrensburg@gmail.com>
2025-07-04 11:03:58 +0200

    added gdscript stuff

---
+++ .config/nvim/after/ftplugin/gdscript.lua
@@ -0,0 +1,8 @@
+-- Set tabstop to 2 spaces
+vim.opt_local.tabstop = 2
+
+-- Set shiftwidth (auto-indent) to 2 spaces
+vim.opt_local.shiftwidth = 2
+
+-- Use spaces instead of tabs
+vim.opt_local.expandtab = true
