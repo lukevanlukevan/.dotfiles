6d45d924b369ce5b5950dd4fe26479035cb7096c
lukevanlukevan <lukevrensburg@gmail.com>
2025-07-07 17:50:18 +0200

    fix tabs and add formatter

--- .config/nvim/after/ftplugin/gdscript.lua
+++ .config/nvim/after/ftplugin/gdscript.lua
@@ -1,8 +1,9 @@
 -- Set tabstop to 2 spaces
 vim.opt_local.tabstop = 2
 
--- Set shiftwidth (auto-indent) to 2 spaces
-vim.opt_local.shiftwidth = 2
+-- Set shiftwidth (auto-indent) to 2 spaces - Disable if using tabs not spaces
+-- otherwise we end up with autoindent that doesnt match
+-- vim.opt_local.shiftwidth = 2
 
 -- Use spaces instead of tabs
-vim.opt_local.expandtab = true
+vim.opt_local.expandtab = false
--- .config/nvim/lua/plugins/conform.lua
+++ .config/nvim/lua/plugins/conform.lua
@@ -3,12 +3,19 @@ return {
   optional = true,
   opts = {
     formatters_by_ft = {
+      gdscript = { "gdformat" },
       javascript = { "prettier" },
       javascriptreact = { "prettier" },
       typescript = { "prettier" },
       typescriptreact = { "prettier" },
       jsx = { "prettier" },
       tsx = { "prettier" },
+    },
+    formatters = {
+      gdformat = {
+        command = "gdformat",
+        stdin = false, -- must be false, or unset (default)
+      },
     },
   },
 }
